3fac5a88776672fa36a0dba18d07318c6e8c44aa
Kimberly Aviles <01010110.v@gmail.com>
2020-03-26 18:07:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,30 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    newly_created_song = Song.new
+    newly_created_song.save
+    newly_created_song
+  end
+
+  def self.new_by_name(name_string)
+    newly_created_song = Song.new
+    newly_created_song.name = name_string
+    newly_created_song
+  end
+
+  def self.create_by_name(name_string)
+    newly_created_song = Song.new
+    newly_created_song.name = name_string
+    newly_created_song.save
+    newly_created_song
+  end
+
+  def self.find_by_name(name_string)
+    array = @@all
+    array.find do |iteration|
+      iteration.name == name_string
+    end
+  end
+
 end
