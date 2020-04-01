12a33de706b86f2bf3765f235cc28dc1565b1c85
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-01 16:53:37 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,64 @@ class Song
   end
 
   def self.create
-    @@all << self.new
+    new_song = Song.new
+    new_song.save
+    new_song
   end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    variable = new_by_name(name)
+    variable.save
+    variable
+  end
+
+  def self.find_by_name(song_name)
+    @@all.find {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name (song_name)
+    if self.find_by_name(song_name)
+      return self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(file_name)
+    new_file_name = file_name.split(" - ")
+    new_new_file_name = new_file_name[1].split(".")
+    song_name = new_new_file_name[0]
+    variable_song = Song.new
+    variable_song.name = song_name
+    artist_name = new_file_name[0]
+    variable_song.artist_name = artist_name
+    variable_song
+  end
+
+  def self.create_from_filename(file_name)
+    new_file_name = file_name.split(" - ")
+    new_new_file_name = new_file_name[1].split(".")
+    song_name = new_new_file_name[0]
+    variable_song = Song.new
+    variable_song.name = song_name
+    artist_name = new_file_name[0]
+    variable_song.artist_name = artist_name
+    variable_song.save
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
 
 end
